2698d54e581101e0d38a6b76d033cbf7fc36d455
jumozaga <jumozaga@yahoo.com>
2023-01-08 17:13:13 -0300

    Modificações

---
+++ antigos/hook/list.js
@@ -0,0 +1,35 @@
+<ol class="pokemons">
+        <li>Pikachu
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg" alt="">    
+        </li>
+        <li>Bulbasaur
+          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="">  
+        </li>
+        <li>Ivysaur
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg" alt="">
+        </li>
+        <li>Venusaur
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg" alt="">
+
+        </li>
+        <li>Charmander
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg" alt="">
+
+        </li>
+        <li>Charmeleon
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/5.svg" alt="">
+
+        </li>
+        <li>Charizard
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/6.svg" alt="">
+
+        </li>
+        <li>Squirtle
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/7.svg" alt="">
+
+        </li>
+        <li>Wartortle
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/8.svg" alt="">
+
+        </li>
+    </ol>
